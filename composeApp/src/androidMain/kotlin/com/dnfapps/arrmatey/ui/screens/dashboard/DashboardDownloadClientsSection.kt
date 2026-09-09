7ad970decd3cb2509aecbd06bb80cf8ce53915da
Owen LeJeune <omlejeune@gmail.com>
2026-09-08 21:07:01 -0400

    update dashboard download client progress track colour

--- composeApp/src/androidMain/kotlin/com/dnfapps/arrmatey/ui/screens/dashboard/DashboardDownloadClientsSection.kt
+++ composeApp/src/androidMain/kotlin/com/dnfapps/arrmatey/ui/screens/dashboard/DashboardDownloadClientsSection.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.trace
 import com.dnfapps.arrmatey.arr.state.CombinedDashboardState
 import com.dnfapps.arrmatey.compose.utils.bytesAsFileSizeString
 import com.dnfapps.arrmatey.shared.MR
@@ -202,6 +203,7 @@ fun DashboardDownloadClientsSection(
                         LinearProgressIndicator(
                             progress = { download.progress.toFloat() },
                             modifier = Modifier.fillMaxWidth().height(4.dp).clip(CircleShape),
+                            trackColor = MaterialTheme.colorScheme.surfaceContainerHighest
                         )
                     }
                 }
