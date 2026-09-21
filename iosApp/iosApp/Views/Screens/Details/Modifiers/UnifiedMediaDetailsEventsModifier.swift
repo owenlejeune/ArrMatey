3c76651aa73516295cde0415c0b333507744401a
Owen LeJeune <omlejeune@gmail.com>
2026-09-21 13:33:00 -0400

    fix(ios): prevent initial delete movie file error toast from triggering on media details open

--- iosApp/iosApp/Views/Screens/Details/Modifiers/UnifiedMediaDetailsEventsModifier.swift
+++ iosApp/iosApp/Views/Screens/Details/Modifiers/UnifiedMediaDetailsEventsModifier.swift
@@ -15,39 +15,31 @@ struct UnifiedMediaDetailsEventsModifier: ViewModifier {
 
     func body(content: Content) -> some View {
         content
-            .onReceive(viewModel.$lastSearchResult) { newVal in
+            .onReceive(viewModel.$lastSearchResult.dropFirst()) { newVal in
                 if let result = newVal {
                     onToast(result ? MR.strings().search_queued.localized() : MR.strings().search_error.localized())
                 }
             }
-            .onReceive(viewModel.$editSuccessTrigger.dropFirst()) { val in
-                if val {
-                    onToast(MR.strings().item_edited_successfully.localized())
-                    onEditSuccess()
+            .onReceive(viewModel.$editSuccessTrigger.dropFirst()) { _ in
+                onToast(MR.strings().item_edited_successfully.localized())
+                onEditSuccess()
+            }
+            .onReceive(viewModel.$editErrorTrigger.dropFirst()) { _ in
+                onToast(MR.strings().error_editing_item.localized())
+            }
+            .onReceive(viewModel.$deleteSuccessTrigger.dropFirst()) { _ in
+                onToast(MR.strings().item_deleted_successfully.localized())
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                    onDismiss()
                 }
             }
-            .onReceive(viewModel.$editErrorTrigger.dropFirst()) { val in
-                if val {
-                    onToast(MR.strings().error_editing_item.localized())
-                }
+            .onReceive(viewModel.$deleteErrorTrigger.dropFirst()) { _ in
+                onToast(MR.strings().error_deleting_item.localized())
             }
-            .onReceive(viewModel.$deleteSuccessTrigger.dropFirst()) { val in
-                if val {
-                    onToast(MR.strings().item_deleted_successfully.localized())
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                        onDismiss()
-                    }
-                }
-            }
-            .onReceive(viewModel.$deleteErrorTrigger.dropFirst()) { val in
-                if val {
-                    onToast(MR.strings().error_deleting_item.localized())
-                }
-            }
-            .onReceive(viewModel.$deleteMovieFileSuccessTrigger) { _ in
+            .onReceive(viewModel.$deleteMovieFileSuccessTrigger.dropFirst()) { _ in
                 onToast(MR.strings().item_deleted_successfully.localized())
             }
-            .onReceive(viewModel.$deleteMovieFileErrorTrigger) { _ in
+            .onReceive(viewModel.$deleteMovieFileErrorTrigger.dropFirst()) { _ in
                 onToast(MR.strings().error_deleting_item.localized())
             }
     }
