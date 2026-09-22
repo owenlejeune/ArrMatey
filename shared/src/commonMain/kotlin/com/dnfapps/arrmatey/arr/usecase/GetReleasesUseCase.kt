476dcebdfcb97775b3973a5e3dee3b9fe154953e
Owen LeJeune <omlejeune@gmail.com>
2026-09-22 12:34:13 -0400

    lint

--- shared/src/commonMain/kotlin/com/dnfapps/arrmatey/arr/usecase/GetReleasesUseCase.kt
+++ shared/src/commonMain/kotlin/com/dnfapps/arrmatey/arr/usecase/GetReleasesUseCase.kt
@@ -18,7 +18,6 @@ import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.flowOf
 
 class GetReleasesUseCase(
     private val instanceManager: InstanceManager,
